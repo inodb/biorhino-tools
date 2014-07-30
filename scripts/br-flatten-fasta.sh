8d5554af0f744e3f3cda2196a83b06ad9513a63a
Ino de Bruijn <ino.debruijn@scilifelab.se>
2014-07-30 14:08:01 +0200

    Make it a little easier to pipe to flatten-fasta with <(cat -)

--- scripts/br-flatten-fasta.sh
+++ scripts/br-flatten-fasta.sh
@@ -33,9 +33,8 @@ then
     exit 1
 fi
 
-less $1 | \
 awk -v RS='>' \
     -v FS="\n" \
     -v OFS="" \
     -v ORS="" \
-    '{ if (NR > 1) { printf ">%s\n",$1; $1=""; printf "%s\n",$0 } }'
+    '{ if (NR > 1) { printf ">%s\n",$1; $1=""; printf "%s\n",$0 } }' $1
